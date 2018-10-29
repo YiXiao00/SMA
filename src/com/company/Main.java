a02a8b63e4268f66e048208e0146c85955b4ac70
dk17523 <dkenna99@gmail.com>
2018-10-29 11:51:41 +0000

    javaFile

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class mongoInterface {
+
+    public void addDocument(String json){
+
+    }
+
+    public void searchDocument(int id){
+
+    }
+
+    public void deleteDocument(int id){
+
+    }
+
+}
