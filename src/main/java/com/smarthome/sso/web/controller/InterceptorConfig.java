820ba1716056831e098c03bc0e83c0cdd53a2629
sc17141 <sc17141@bristol.ac.uk>
2019-02-04 12:43:57 +0000

    Add Interceptor
    define patterns at the config class!!

---
+++ src/main/java/com/smarthome/sso/web/controller/InterceptorConfig.java
@@ -0,0 +1,23 @@
+package com.smarthome.sso.web.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public SigninInterceptor getSigninInterceptor() {
+        return new SigninInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getSigninInterceptor()).addPathPatterns("/user/test");
+        super.addInterceptors(registry);
+    }
+}
+
+
---
+++ src/main/java/com/smarthome/sso/web/controller/SigninInterceptor.java
@@ -0,0 +1,40 @@
+package com.smarthome.sso.web.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SigninInterceptor extends HandlerInterceptorAdapter {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null){
+            String inputSession = "";
+            for (Cookie cookie: cookies){
+                if ("sessionId".equals(cookie.getName())){
+                    inputSession = cookie.getValue();
+                    break;
+                }
+            }
+            if (!"".equals(inputSession)){
+                String record = (String)redisTemplate.opsForValue().get(inputSession);
+                if (inputSession.equals(record)){
+                    return true;    //already signed in
+                }
+
+            }
+        }
+        //not signed in
+        response.sendRedirect("/homepage");
+        return true;
+    }
+}
